7350d9074cd8eb8df61a908f47ebaf3d16d06e6a
aj <prajitdatta15@gmail.com>
2026-07-21 19:49:19 +0530

    Remove duplicate LockScreen wrapper from page.tsx so only RootLayout holds the gatekeeper Lockscreen

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,5 @@
 import { AppShell } from "@/components/shell/AppShell";
-import LockScreen from "@/core/components/LockScreen";
 
 export default function Home() {
-  return (
-    <LockScreen>
-      <AppShell />
-    </LockScreen>
-  );
+  return <AppShell />;
 }
